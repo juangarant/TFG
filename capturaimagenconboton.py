7ba5a0d96aab5486a2211a6a4261343ce05c8c08
Juan García <juan.garcia.anton@gmail.com>
2025-08-01 18:50:30 +0200

    Código captura imágenes dataset

---
+++ capturaimagenconboton.py
@@ -0,0 +1,49 @@
+# Untitled - By: juang - Fri Jul 25 2025
+
+import sensor
+import time
+import machine
+import os
+import gc
+
+# Configuración
+LABEL = "nocascodificil" #Nombre de la carpeta donde se guardan las imagenes
+PIXFORMAT = sensor.GRAYSCALE
+FRAMESIZE = sensor.QVGA #Tamaño de la imagen (QVGA=320x240)
+BUTTON_PIN = "D14" #Pin de la portenta donde está conectado el botón
+
+# Inicialización cámara
+sensor.reset()
+sensor.set_pixformat(PIXFORMAT)
+sensor.set_framesize(FRAMESIZE)
+sensor.skip_frames(time=2000)
+
+# LED
+led = machine.LED("LED_BLUE")
+led.off()
+
+# Botón
+button = machine.Pin(BUTTON_PIN, machine.Pin.IN, machine.Pin.PULL_UP)
+index = 0
+
+# Crear carpeta si no existe
+if LABEL not in os.listdir():
+    os.mkdir(LABEL)
+
+# Bucle principal
+print("Listo. Capturando imágenes. Pulsa el botón para guardar.")
+
+while True:
+    img = sensor.snapshot()
+    #print("%d" % (button.value()))  # Muestra el estado del botón
+
+    if button.value() == 1:  # pulsado
+        timestamp = time.ticks_ms()
+        filename = "%s/img-%d_%d.jpg" % (LABEL, index, timestamp)
+        img.save(filename)
+        led.on()
+        print("Imagen guardada:", filename)
+        time.sleep_ms(300)  # evita guardar varias seguidas por un solo toque
+        led.off()
+        index += 1
+        gc.collect()
